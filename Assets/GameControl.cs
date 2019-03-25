25e6ae7c499b09f0143e61e15fa313d4ae34d05b
Jerry <goo1140@gmail.com>
2019-03-25 10:03:58 -0400

    Progress 1
    Background, Bird, Score text, and game over text.

---
+++ Assets/GameControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour{
+    
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool isGameOver = false;
+    
+    // Start is called before the first frame update
+    void Awake(){
+        if(instance == null){
+            instance = this;
+        }else if(instance != this){
+            Destroy (gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+    }
+    
+    public void BirdDied(){
+        gameOverText.SetActive(true);
+        isGameOver = true;
+    }
+}
